d22169021825d0a625e2e19b17428f09dfb76b9e
imkunet <kunettyi@gmail.com>
2023-03-16 18:17:31 -0400

    rayon

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 use std::time::Instant;
 
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+
 fn main() {
     let start = Instant::now();
     let mut current = 0.0;
-    let mut target: u32 = 10;
-    let mut x: u32 = 0;
-    let mut n: u32 = 0;
+    let mut target: u64 = 10;
+    let mut x: u64 = 0;
+    let mut n: u64 = 0;
 
     loop {
-        if n == target {
-            println!(
-                "n = 10E{:<4} | {:16} | {:.16}",
-                x,
-                format!("{:#?}", Instant::now().duration_since(start)),
-                current * 4.0
-            );
-            x += 1;
-            target *= 10;
-        }
+        current += (n..target)
+            .into_par_iter()
+            .map(|n| (-1.0_f64).powf(n as f64) / (2 * n + 1) as f64)
+            .sum::<f64>();
 
-        current += (-1.0_f64).powf(n as f64) / (2 * n + 1) as f64;
-        n += 1;
+        println!(
+            "n = 10E{:<4} | {:16} | {:.16}",
+            x,
+            format!("{:#?}", Instant::now().duration_since(start)),
+            current * 4.0
+        );
+        x += 1;
+        n = target;
+        target *= 10;
     }
 }
